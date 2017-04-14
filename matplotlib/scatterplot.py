20fc8c221313241a6b928a7577d05388960f9893
rahul.raviprasad <rahul.raviprasad@gmail.com>
2017-04-14 16:06:56 +0530

    added c, cmap explanation for scatter

--- matplotlib/scatterplot.py
+++ matplotlib/scatterplot.py
@@ -62,10 +62,19 @@ y_index = 1
 # this formatter will label the colorbar with the correct target names
 formatter = plt.FuncFormatter(lambda i, *args: iris.target_names[int(i)])
 
+# For the scatter method here we are passing x, y, c and cmap.
+# c : an array(sequence of floats or numbers)
+# cmap: A Colormap instance or registered name. cmap is only used if c is an array of floats. If None, defaults to rc image.cmap.
+# matplotlib.pyplot.scatter(x, y, c=sequenceOfN_NumberToBeMapped, cmap=None)
+
+# iris.data[:, x_index] create a new view with all 150 records and particular dimension, and same for y
 plt.scatter(iris.data[:, x_index], iris.data[:, y_index], c=iris.target, cmap=plt.get_cmap('RdYlBu', 3))
 
 plt.colorbar(ticks=[0, 1, 2], format=formatter)
+# set colour limits of current image
 plt.clim(-0.5, 2.5)
+
+# the label strings to be displayed
 plt.xlabel(iris.feature_names[x_index])
 plt.ylabel(iris.feature_names[y_index])
 
