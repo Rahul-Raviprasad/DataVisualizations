3cb0f4baf8322a69a4667db68e00e147cad3b635
rahul.raviprasad <rahul.raviprasad@gmail.com>
2018-04-18 12:57:01 +0530

    added stackplot example

--- matplotlib/test.py
+++ matplotlib/test.py
@@ -1,14 +1,22 @@
 import matplotlib.pyplot as plt
 
-x = [1,2,6,7,23,12,22,3]
-y = [3,4,5,23,5,6,7,12]
+x = [1,2,6,7]
+y1 = [3,4,5,23]
+y2 = [4,6,7,2]
+y3 = [20,3,10,2]
 
-# marker generally is a dot, to say show a star you need to use marker='*', s is for size
-plt.scatter(x, y, color='k', marker='*', s=500)
+# Empty list are used because we really dont want to draw any plots. We simply need them for legends
+# this is because stackplot doesn't support how to draw a legend
+plt.plot([],[], label='y1', linewidth=5, color='k')
+plt.plot([],[], label='y2', linewidth=5, color='r')
+plt.plot([],[], label='y3', linewidth=5, color='b')
+
+plt.stackplot(x, y1,y2,y3, colors=['k', 'r', 'b'])
 
 plt.xlabel('x')
 plt.ylabel('y')
+plt.legend()
 
-plt.title('Scatter Plot')
+plt.title('Stack Plot')
 
 plt.show()
