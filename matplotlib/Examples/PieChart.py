66eead2d0b82f03153d17d2fa5124c323f4c4a59
rahul.raviprasad <rahul.raviprasad@gmail.com>
2018-04-19 11:41:30 +0530

    adding more options to the pie chart

--- matplotlib/Examples/PieChart.py
+++ matplotlib/Examples/PieChart.py
@@ -2,6 +2,11 @@ import matplotlib.pyplot as plt
 
 slices = [1,2,6,7]
 fruits = ['bananas', 'apples', 'oranges', 'watermelons']
-plt.pie(slices, labels=fruits, colors=['k', 'r', 'b', 'g'])
+plt.pie(slices,
+        labels=fruits,
+        colors=['k', 'r', 'b', 'g'],
+        startangle=90,
+        shadow=True,
+        explode=(0,0.1,0,0))
 plt.title('Fruits')
 plt.show()
